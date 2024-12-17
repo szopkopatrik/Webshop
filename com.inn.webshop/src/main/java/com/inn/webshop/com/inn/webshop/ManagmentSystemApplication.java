6f09ae4f69d2b25992af882cc290651dc5010de0
Szopkó_Endre_Patrik <szep@mailbox.unideb.hu>
2024-12-17 15:02:54 +0100

    Added user Model

---
+++ com.inn.webshop/src/main/java/com/inn/webshop/com/inn/webshop/ManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inn.webshop.com.inn.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ com.inn.webshop/src/main/java/com/inn/webshop/com/inn/webshop/rest/UserRest.java
@@ -0,0 +1,4 @@
+package com.inn.webshop.com.inn.webshop.rest;
+
+public interface UserRest {
+}
